45cce23dd8e9f3830fa046d3c3bb8ff893516179
João Vareiro <joao.vareiro@ufms.br>
2021-12-15 22:55:01 -0400

    minor fixes

--- TECH-DIVE JAVA/src/main/java/M1S05/Ex5/Pacote1/Parte1.java
+++ TECH-DIVE JAVA/src/main/java/M1S05/Ex5/Pacote1/Parte1.java
@@ -1,13 +1,15 @@
-package M1S05.Ex05.Pacote1;
-
-import Pacote2.Parte2;
-
-public class Parte1 {
-    public static void main(String[] args) {
-        Pacote2.Parte2 a = new Pacote2.Parte2();
-        Pacote2.Parte2.retangulo b = a.new retangulo(5,3);
-        b.imprimeInfo();
-        Parte2.medeArea(2,5);
-    }
-}
+//package M1S05.Ex05.Pacote1;
+//
+////import Pacote2.Parte2;
+//
+//import M1S05.Ex5.Pacote2.Parte2;
+//
+//public class Parte1 {
+//    public static void main(String[] args) {
+//        Pacote2.Parte2 a = new Pacote2.Parte2();
+//        Pacote2.Parte2.retangulo b = a.new retangulo(5,3);
+//        b.imprimeInfo();
+//        Parte2.medeArea(2,5);
+//    }
+//}
 
